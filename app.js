d5ed963fa770cf12d85d579d639d1756df841d4b
Dinusha <dinusha.kannangara@tetherfi.com>
2021-06-02 10:04:02 +0530

    add exception handling

--- app.js
+++ app.js
@@ -5,16 +5,27 @@ const io = require('socket.io')(server);
 const clients = {};
 
 io.on('connection', client => {
-  client.on('reply', data => { 
-    if(data.event === "register"){
-        clients[data.myId] = client;        
+  client.on('reply', data => {
+    if (data.event === "register") {
+      if (data.myId) {
+        clients[data.myId] = client;
+      }
 
     }
-      if(data.event === "send"){
+    if (data.event === "send") {
+      try {
+        if (clients[data.to]) {
           clients[data.to].emit("event", data);
+        }
+      } catch (ex) {
+        console.error(ex);
       }
-   });
+
+    }
+  });
   client.on('disconnect', () => { /* … */ });
 });
 
 server.listen(3100);
+
+console.log('Server Started on Port 3100');
